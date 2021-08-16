4c581633fa51d5a0d7723b6ca85cd2c6f7821d76
iLuiizUHD <luizgomes749@gmail.com>
2021-08-15 22:27:14 -0300

    Agora conecta no mongo

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,6 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { SoilData } from '../../models';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
-	res.status(200).json({ success: true });
-}
+  const resdvb = new SoilData({ ...req.body });
+  resdvb.save();
+
+  res.status(201).send('BUCETA');
+};
