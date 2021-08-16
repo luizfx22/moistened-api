51c2289439219743b90386b1f6d4b875a1f0d490
Luiz <41802882+iLuiizUHD@users.noreply.github.com>
2021-08-15 22:33:10 -0300

    Delete data.ts

--- pages/api/data.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { SoilData } from '../../models';
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const resdvb = new SoilData({ ...req.body });
-  resdvb.save();
-
-  res.status(201).send('BUCETA');
-};
