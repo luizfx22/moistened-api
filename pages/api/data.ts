1c152843c089328377689e0fe0a8f0d88d43a313
iLuiizUHD <luizgomes749@gmail.com>
2021-08-30 18:36:36 -0300

    Validação e cadastro no DB dos dados vindos do sensor

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,9 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import * as yup from 'yup';
 import { SoilData } from '../../models';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const resdvb = new SoilData({ ...req.body });
-  resdvb.save();
+const validator = yup.object().shape({
+  air_temperature: yup.number().required(),
+  air_humidity: yup.number().required(),
+  soil_humidity: yup.number().required(),
+  readed_by: yup.string().required(),
+  readed_at: yup.date().required(),
+});
 
-  res.status(201).send('SALVOU!');
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { body } = req;
+
+    const isValid = validator.isValidSync(body);
+
+    if (!isValid) {
+      return res.status(400).json({ message: 'Body content is invalid!' });
+    }
+
+    new SoilData(body).save();
+
+    return res.status(201).send('SALVOU!');
+
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
