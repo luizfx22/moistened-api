332ae7bf73d9f07e240333cd23570a599768a022
iLuiizUHD <luizgomes749@gmail.com>
2021-09-07 12:34:36 -0300

    Início gravação dos dados no supabase

--- pages/api/sensor/register.ts
+++ pages/api/sensor/register.ts
@@ -1,4 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@supabase/supabase-js";
+import Sensor from "~/types/Sensor";
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON);
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
 	console.log(req.body);
